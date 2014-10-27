d76591b01746256f223971d7144063609a07c815
stacybanks514 <stacybanks514@gmail.com>
2014-10-27 15:12:25 -0400

    Added columns to users table
    t.string  "password_digest"
    t.string  "remember_token"
    t.boolean "admin"

--- Shelf_It/db/schema.rb
+++ Shelf_It/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141027154750) do
+ActiveRecord::Schema.define(version: 20141027190512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,11 @@ ActiveRecord::Schema.define(version: 20141027154750) do
   end
 
   create_table "users", force: true do |t|
-    t.string "name"
-    t.string "email"
+    t.string  "name"
+    t.string  "email"
+    t.string  "password_digest"
+    t.string  "remember_token"
+    t.boolean "admin"
   end
 
 end
