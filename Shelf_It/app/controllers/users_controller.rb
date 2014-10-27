503c14fba9357ecbd63ab7440934f5d7de2b4524
stacybanks514 <stacybanks514@gmail.com>
2014-10-27 15:24:24 -0400

    Adding to the users controller

--- Shelf_It/app/controllers/users_controller.rb
+++ Shelf_It/app/controllers/users_controller.rb
@@ -20,6 +20,17 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
 private
 
   def users_params
