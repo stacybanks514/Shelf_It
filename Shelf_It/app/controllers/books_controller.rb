c24ccde2749270403f41797da5904f6dfd81fd86
stacybanks514 <stacybanks514@gmail.com>
2014-10-28 09:57:50 -0400

    Got books/index.html to display user’s book shelf. Added the search route and function on books

--- Shelf_It/app/controllers/books_controller.rb
+++ Shelf_It/app/controllers/books_controller.rb
@@ -1,5 +1,10 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
 end
--- Shelf_It/config/routes.rb
+++ Shelf_It/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # get 'welcome/index'
 resources :users
-resources :books
+resources :books do
+    get 'search', on: :collection
+  end 
+
 resources :sessions, only: [:new, :create]
 delete '/signout', to: 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
