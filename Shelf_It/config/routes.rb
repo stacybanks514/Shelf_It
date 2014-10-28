ad75e7910c72bef40e4a048ab1306687a0dd864c
stacybanks514 <stacybanks514@gmail.com>
2014-10-28 08:49:14 -0400

    Added resources :books, views/books/index.html, controllers/books_controller.rb

--- Shelf_It/config/routes.rb
+++ Shelf_It/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # get 'welcome/index'
 resources :users
-
+resources :books
 resources :sessions, only: [:new, :create]
 delete '/signout', to: 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
